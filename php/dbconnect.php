d159dca453f57ed6a99a81b3d7dcea3d43ec204d
321k <erikjohansson01@gmail.com>
2016-05-30 22:03:02 +0100

    added sign in and registration

---
+++ php/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+if(!mysql_connect("localhost","erik","johansson"))
+{
+     die('oops connection problem ! --> '.mysql_error());
+}
+if(!mysql_select_db("apollo"))
+{
+     die('oops database selection problem ! --> '.mysql_error());
+}
+?>
