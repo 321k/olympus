32f4fd69efe02a8e7266356a5f35d10b80ba965a
321k <erikjohansson01@gmail.com>
2016-07-10 17:33:06 +0300

    Added R script for lynx automation

--- php/get_gt_data.php
+++ php/get_gt_data.php
@@ -7,4 +7,7 @@ if(!isset($_SESSION['user']))
 }
 
 exec("Rscript /var/www/html/olympus/r/fetch_gt_data.R");
+
+$res=mysql_query("select * from gt_urls join search_volume on search_volume.gt_urls_id = gt_urls.id where gt_urls.user_id =".$_SESSION['user']);
+echo $res
 ?>
